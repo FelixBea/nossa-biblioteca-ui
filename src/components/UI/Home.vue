39d74b1912443fcf1008333cffd37039df3142bb
FelixBea <bfelix@live.com>
2020-08-19 09:53:28 -0300

    Adicionando biblioteca de UI Vuetify

--- src/components/UI/Home.vue
+++ src/components/UI/Home.vue
@@ -2,7 +2,39 @@
   <div class="container">
     <Header/>
     <main>
-      <router-view />
+      <v-tabs v-model="tab" align-with-title>
+        <v-tab>
+          Livros
+        </v-tab>
+        <v-tab>
+          Autores
+        </v-tab>
+        <v-tab>
+          Editoras
+        </v-tab>
+      </v-tabs>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>aaaaaaa</v-card-text>
+            <router-view />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>aaaaadddddaa</v-card-text>
+            <router-view />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>aaagggggaaaa</v-card-text>
+            <router-view />
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+
     </main>
     <Footer/>
   </div>
@@ -14,7 +46,12 @@ import Footer from './Footer.vue'
 
 export default {
   name: 'Home',
-  components: { 'Header': Header, 'Footer': Footer }
+  components: { 'Header': Header, 'Footer': Footer },
+  data () {
+    return {
+      tab: null
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
 import App from './App'
 import 'normalize.css'
 import router from './router'
@@ -11,6 +12,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  vuetify,
   components: { App },
   template: '<App/>'
 })
